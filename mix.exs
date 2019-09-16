7e12bcab58eb8aceeef0c35e1ed2d3674842dddb
dway8 <diane.maniere@gmail.com>
2019-09-16 22:50:08 +0200

    fix test database

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Aisf.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.drop", "ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,7 @@
 ExUnit.start()
 Faker.start()
 
+Ecto.Adapters.SQL.Sandbox.mode(Aisf.Repo, :manual)
 {:ok, _} = Application.ensure_all_started(:wallaby)
 
 Application.put_env(:wallaby, :base_url, AisfWeb.Endpoint.url())
